36361a22afc5ad7eeebe64450880ca18cc5a2d93
Googleplex1999 <94068755+Googleplex1999@users.noreply.github.com>
2022-04-02 21:47:51 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "fe9aa5be-5ad3-491d-b2fd-bb40e10480f3",
                         "alterId": 0
                     }
                 ],
